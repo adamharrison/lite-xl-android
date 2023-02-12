57229bedebd4f35b14fdb0db2804d6984b68cc09
Adam Harrison <adamdharrison@gmail.com>
2023-02-12 14:30:30 -0500

    Updated build to work with new fucked up way of including libraries.

--- build.sh
+++ build.sh
@@ -6,16 +6,17 @@
 : ${ANDROID_ARCH=26}
 : ${BIN=lite-xl.apk}
 : ${APP=com.litexl.litexl}
+SDL="lib/lite-xl-simplified/lib/SDL"
 
-BUILD_TYPE=Release && BUILD_FOLDER=release
-[[ "$@" == *"-g"* ]] && BUILD_TYPE=Debug && BUILD_FOLDER=debug
+BUILD_TYPE=Debug && BUILD_FOLDER=debug
+[[ "$@" == *"-O"* ]] && BUILD_TYPE=Release && BUILD_FOLDER=release
 [[ ! -d $ANDROID_NDK_HOME ]] && echo "Please define \$ANDROID_NDK_HOME." && exit -1
-[[ ! -d "lib/lite-xl-simplified" || ! -d "lib/lite-xl-simplified/lib/SDL" ]] && echo "Please run `git submodule update --init --recursive`." && exit -1
+[[ ! -d $SDL ]] && echo "Please run `git submodule update --init --recursive`." && exit -1
 
 [[ "$@" == "clean" ]] && rm -rf $APP $BIN && exit -1
 
 # Copy over the android sample project from SDL, and merge in our changes.
-[[ ! -d $APP ]] && cp -r lib/lite-xl-simplified/lib/SDL/android-project $APP && cp -r template/* $APP
+[[ ! -d $APP ]] && cp -r $SDL/android-project $APP && mkdir -p $APP/app/jni/SDL && cp $SDL/Android.mk $APP/app/jni/SDL && rm -rf $APP/app/jni/src && cp -r template/* $APP
 
 # Install all desired plugins, if any.
 ASSET_FOLDER="$APP/app/src/main/assets" && rm -rf $ASSET_FOLDER/user && mkdir $ASSET_FOLDER/user
@@ -25,15 +26,18 @@ if [[ "$LITEXL_PLUGINS" != "" ]]; then
   ./lpm install $LITEXL_PLUGINS $LPM_ARGUMENTS && ./lpm purge $LPM_ARGUMENTS || { echo "Can't install $@." && exit -1; }
 fi
 
-# Go through, and build liblite.a for each target.
+(cd $APP && ./gradlew build$BUILD_TYPE) || { echo "Can't build gradle." && exit -1; }
+
+# Go through, and build main.so for each target, linking against the SDL we just built, because Android's build process really sucks.
 declare -a TARGETS=("armv7a-linux-androideabi" "i686-linux-android" "aarch64-linux-android" "x86_64-linux-android")
 declare -a JNILIBS=("armeabi-v7a" "x86" "arm64-v8a" "x86_64")
+WD=`pwd`
 for TARGET_IDX in {0..3}; do
   export TARGET=${TARGETS[TARGET_IDX]}
   export JNILIB=${JNILIBS[TARGET_IDX]}
-  if [[ ! -f $APP/app/jni/src/lib/$JNILIB/libmain.so ]]; then
-    (cd lib/lite-xl-simplified && rm -f lite.so && ./build.sh clean && CC=$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/linux-x86_64/bin/$TARGET$ANDROID_ARCH-clang AR=$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/linux-x86_64/bin/llvm-ar BIN='lite.so' LLFLAGS="-g -fPIC" ./build.sh -fPIC -Ilib/SDL/include -shared $@ -DNO_SDL;) || exit -1
-    mkdir -p $APP/app/jni/src/lib/$JNILIB && mv lib/lite-xl-simplified/lite.so $APP/app/jni/src/lib/$JNILIB/libmain.so
+  if [[ ! -f $APP/app/src/main/jniLibs/$JNILIB/libmain.so ]]; then
+    (cd lib/lite-xl-simplified && rm -f lite.so && ./build.sh clean && CC=$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/linux-x86_64/bin/$TARGET$ANDROID_ARCH-clang AR=$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/linux-x86_64/bin/llvm-ar BIN='lite.so' LLFLAGS="-g -fPIC" ./build.sh -L$WD/$APP/app/build/intermediates/ndkBuild/$BUILD_FOLDER/obj/local/$JNILIB -lSDL2 -fPIC -Ilib/SDL/include -lSDL2 -shared $@ -DNO_SDL;) || exit -1
+    mkdir -p $APP/app/src/main/jniLibs/$JNILIB && mv lib/lite-xl-simplified/lite.so $APP/app/src/main/jniLibs/$JNILIB/libmain.so
   fi
 done
 
